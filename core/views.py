88f4e0c8715a95a1c095e0e9de86de03f1a69045
Filipe Medeiros <fcma@cin.ufpe.br>
2018-11-06 17:41:03 -0300

    Design adjustment

--- core/views.py
+++ core/views.py
@@ -7,21 +7,27 @@ from .forms import DoubtForm
 # Create your views here.
 
 def index (request):
-	success = False
-	form = DoubtForm(request.POST or None)
-	if form.is_valid():
-		success = form.send_mail()
-
 	posts = Post.objects.all().order_by('-id')
 
 	context = {
 		'post1' : posts[0],
 		'post2' : posts[1],
-		'post3' : posts[2],
-		'form'	: form,
-		'success': success, 
+		'post3' : posts[2], 
 	}
 	return render(request, 'index.html', context)
+
+
+def doubtView (request):
+	success = False
+	form = DoubtForm(request.POST or None)
+	if form.is_valid():
+		success = form.send_mail()
+
+	context = {
+		'form'	: form,
+		'success': success,
+	}
+	return render (request, 'doubt.html', context)
 
 class PostListView (generic.ListView):
 	queryset = Post.objects.all().reverse()
